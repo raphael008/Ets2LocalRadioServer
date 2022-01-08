28726878d165908f968213be65fb8b627d52cc5b
raphael008 <raphael008@icloud.com>
2022-01-08 11:07:12 +0800

    first usable version

---
+++ MusicThread.cs
@@ -0,0 +1,127 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace LocalRadioServer
+{
+    public class MusicThread
+    {
+        public static byte[] segments;
+        private static Queue<string> songs = new();
+
+        public static async Task SliceMusicSegment()
+        {
+            Console.WriteLine("Music thread is running...");
+            var songList = Directory.EnumerateFiles(@"F:\music - 2", "*.mp3").ToList();
+
+            var random = new Random();
+            songList = songList.OrderBy(s => random.Next()).ToList();
+            songList.ForEach(songs.Enqueue);
+
+            while (true)
+            {
+                var song = songs.Dequeue();
+                Console.WriteLine($"Playing {Path.GetFileNameWithoutExtension(song)}");
+                
+                FileStream fileStream = new(song, FileMode.Open, FileAccess.Read);
+
+                if (ContainsID3Tag(fileStream))
+                {
+                    int tagSize = GetID3TagSize(fileStream);
+                    Console.WriteLine($"{song} has ID3, size: {tagSize}");
+
+                    // read and remove ID3 tag bytes from the stream
+                    byte[] tagBytes = new byte[tagSize];
+                    fileStream.Read(tagBytes, 0, tagBytes.Length);
+                }
+                else
+                {
+                    Console.WriteLine($"{song} no ID3");
+                }
+
+                int bitRate = GetBitRate(fileStream);
+                Console.WriteLine($"bit rate: {bitRate}");
+                fileStream.Close();
+
+                fileStream = new(song, FileMode.Open, FileAccess.Read);
+                long bytesToRead = fileStream.Length;
+                while (bytesToRead > 0)
+                {
+                    // 8K per second
+                    segments = new byte[8 * 1024];
+                    int bytesRead = fileStream.Read(segments, 0, segments.Length);
+                    bytesToRead -= bytesRead;
+
+                    Program.Signal.Set();
+                    // bitRate Kbps /8 =KBps
+                    
+                    Thread.Sleep((int) Math.Floor((double) (1000 / (bitRate / 8 / 8))));
+
+                    // Console.WriteLine($"bytesToRead:{bytesToRead} bytesRead:{bytesRead}");
+                }
+                
+                songs.Enqueue(song);
+                fileStream.Close();
+            }
+        }
+
+        private static bool ContainsID3Tag(FileStream fileStream)
+        {
+            byte[] buffer = new byte[3];
+            fileStream.Read(buffer, 0, buffer.Length);
+            if (Encoding.UTF8.GetString(buffer) == "ID3")
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static int GetID3TagSize(FileStream fileStream)
+        {
+            byte[] buffer = new byte[7];
+            fileStream.Read(buffer, 0, buffer.Length);
+            return BitConverter.ToInt32(buffer.Skip(3).Take(4).Reverse().ToArray());
+        }
+
+        private static int GetBitRate(FileStream fileStream)
+        {
+            Dictionary<string, int> bitRateIndexTable = new Dictionary<string, int>();
+            bitRateIndexTable.Add("0001", 32);
+            bitRateIndexTable.Add("0010", 40);
+            bitRateIndexTable.Add("0011", 48);
+            bitRateIndexTable.Add("0100", 56);
+            bitRateIndexTable.Add("0101", 64);
+            bitRateIndexTable.Add("0110", 80);
+            bitRateIndexTable.Add("0111", 96);
+            bitRateIndexTable.Add("1000", 112);
+            bitRateIndexTable.Add("1001", 128);
+            bitRateIndexTable.Add("1010", 160);
+            bitRateIndexTable.Add("1011", 192);
+            bitRateIndexTable.Add("1100", 224);
+            bitRateIndexTable.Add("1101", 256);
+            bitRateIndexTable.Add("1110", 320);
+            
+            byte[] buffer = new byte[1024];
+            fileStream.Read(buffer, 0, buffer.Length);
+
+            StringBuilder builder = new StringBuilder();
+            foreach (var b in buffer)
+            {
+                builder.Append(Convert.ToString(b, 2));
+            }
+
+            Regex pattern = new Regex("1{11}1101[0-9]{17}");
+            Match result = pattern.Match(builder.ToString());
+            string mp3Header = result.Value;
+            Console.WriteLine($"mp3 header: {mp3Header}");
+            
+            string bitRateIndex = mp3Header.Substring(16, 4);
+            Console.WriteLine($"bit rate index: {bitRateIndex}");
+
+            int bitRate = 0;
+            bitRateIndexTable.TryGetValue(bitRateIndex, out bitRate);
+
+            return bitRate;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,99 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections.Specialized;
+using System.Net;
+
+namespace LocalRadioServer
+{
+    public static class Program
+    {
+        public static AutoResetEvent Signal = new(false);
+
+        private static HttpListener server;
+        private static readonly string url = "http://127.0.0.1:8888/";
+
+        private static Dictionary<string, HttpListenerContext> clients = new();
+
+        private static int bytesToRead;
+
+        public static void Main(string[] args)
+        {
+            Task.Run(MusicThread.SliceMusicSegment);
+            
+            server = new HttpListener();
+            server.Prefixes.Add(url);
+            server.Start();
+            
+            Console.WriteLine($"Listening on {url}");
+            
+            Task task = HandleRequests();
+            task.GetAwaiter().GetResult();
+            
+            server.Close();
+        }
+
+        private static void HandleRequestsAsync(HttpListenerContext context)
+        {
+            HttpListenerRequest request = context.Request;
+            HttpListenerResponse response = context.Response;
+            NameValueCollection headers = request.Headers;
+
+            foreach (var header in headers)
+            {
+                if ("icy-metadata" == header.ToString()?.ToLower())
+                {
+                    response.AddHeader("icy-metaint", "8192");
+                }
+            }
+
+            while (true)
+            {
+                Signal.WaitOne();
+                
+                byte[] data = new byte[8 * 1024 + 1];
+                Buffer.BlockCopy(MusicThread.segments, 0, data, 0, data.Length - 1);
+                data[8 * 1024] = 0;
+
+                try
+                {
+                    if (response.OutputStream.CanWrite)
+                    {
+                        response.OutputStream.WriteAsync(data, 0, data.Length);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    return;
+                }
+            }
+        }
+
+        public static async Task HandleRequests()
+        {
+            while (true)
+            {
+                Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} waiting new requests...");
+                HttpListenerContext context = await server.GetContextAsync();
+
+                Console.WriteLine($"clients: {clients.Count}");
+                if (clients.ContainsKey(context.Request.UserHostName))
+                {
+                    foreach (var client in clients)
+                    {
+                        if (client.Key == context.Request.UserHostName)
+                        {
+                            client.Value.Response.Close();
+                            clients.Remove(context.Request.UserHostName);
+                        }
+                    }
+                }
+                
+                clients.Add(context.Request.UserHostName, context);
+
+                Console.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} handling new request...");
+                Task.Run(() => HandleRequestsAsync(context));
+            }
+        }
+    }
+}
